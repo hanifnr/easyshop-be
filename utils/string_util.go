943e3a29be13b03190a08582e3b2742520c64870
hanif.nr <hanif.nr@bee.id>
2023-01-10 15:51:43 +0700

    Tambah yaml staging dan fix scraping item pajak 8%

--- utils/string_util.go
+++ utils/string_util.go
@@ -6,6 +6,7 @@ import (
 )
 
 func FormatPrice(text string) string {
+	text = strings.Replace(text, "8%", "", -1)
 	text = strings.Replace(text, "10%", "", -1)
 	text = regexp.MustCompile(`[^a-z0-9, ]+`).ReplaceAllString(text, "")
 	text = strings.Replace(text, " ", "", -1)
