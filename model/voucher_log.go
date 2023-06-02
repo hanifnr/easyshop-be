deea1289253ebb979121ae321685b74155236bee
hanif.nr <hanif.nr@bee.id>
2023-06-02 21:14:49 +0700

    Tambah email customer pada list voucher log

--- model/voucher_log.go
+++ model/voucher_log.go
@@ -1,6 +1,10 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type VoucherLog struct {
 	Id            int64     `json:"id" gorm:"primary_key;auto_increment"`
@@ -10,6 +14,10 @@ type VoucherLog struct {
 	PartnershipId int64     `json:"partnership_id"`
 	RewardAmount  float64   `json:"reward_amount"`
 	RedeemAt      time.Time `json:"redeem_at"`
+	VoucherLogExt `gorm:"-"`
+}
+type VoucherLogExt struct {
+	CustEmail string `json:"cust_email"`
 }
 
 func (VoucherLog VoucherLog) ID() int64 {
@@ -22,4 +30,8 @@ func (VoucherLog) TableName() string {
 
 func (VoucherLog VoucherLog) Validate() error {
 	return nil
+}
+
+func (vl *VoucherLog) SetValueModelExt(db *gorm.DB) {
+	db.Select("email").Table("cust").Where("id = ?", vl.CustId).Scan(&vl.CustEmail)
 }
